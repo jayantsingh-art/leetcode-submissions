5c7e2dbe6f716b8211a7dff77cc84c43461df833
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-07-10 14:59:12 +0530

    Time: 3 ms (13.48%), Space: 41.9 MB (30.29%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int prefix = 0, ans = INT_MAX, j = 0;
+        int prefix = 0, j = 0, ans = INT_MAX;
 
         for(int i = 0; i < nums.size(); i++) {
             prefix += nums[i];
             while(prefix >= target) {
+                ans = min(ans, i-j+1);
                 prefix -= nums[j];
-                ans = min(ans, i - j + 1);
                 j++;
             }
         }
-
-        return (ans == INT_MAX) ? 0 : ans;
+        return ans == INT_MAX ? 0 : ans;
     }
 };
