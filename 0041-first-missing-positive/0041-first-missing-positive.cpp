2de08599be0414cdfdcf7d6fb834bb6d206bbb69
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-07-20 11:56:37 +0530

    Time: 0 ms (100%), Space: 55 MB (86.16%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -4,6 +4,7 @@ public:
         int n = nums.size();
 
         for(int i = 0; i < n; i++) {
+
             while(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
                 swap(nums[i], nums[nums[i] - 1]);
             }
