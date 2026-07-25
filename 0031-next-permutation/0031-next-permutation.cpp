ce54d2adf151fd1cb31b091a9f05cf3b418e47cb
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-07-25 12:01:46 +0530

    Time: 0 ms (100%), Space: 15.8 MB (9.93%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int i = n-1, j = n-1;
+        int i = n-1;
 
         while(i > 0 && nums[i] <= nums[i-1]) {
             i--;
         }
 
         if(i == 0) {
-            return reverse(nums.begin(), nums.end());
+            reverse(nums.begin(), nums.end());
+            return;
         }
-
-        while(j >= i && nums[j] <= nums[i-1]) {
+        
+        int j = n-1;
+        while(nums[j] <= nums[i-1]) {
             j--;
         }
 
         swap(nums[i-1], nums[j]);
+
         reverse(nums.begin() + i, nums.end());
     }
 };
