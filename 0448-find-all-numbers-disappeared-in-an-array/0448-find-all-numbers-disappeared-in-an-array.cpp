6956c786eddbf0fff3281332db75317d9d9c0ff3
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-07-28 16:13:11 +0530

    Time: 3 ms (77.96%), Space: 53.1 MB (57.65%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -5,16 +5,18 @@ public:
         vector<int> ans;
 
         for(int i = 0; i < n; i++) {
-            while(i != nums[i]-1 && nums[i] != nums[nums[i] - 1]) {
-                swap(nums[i], nums[nums[i]-1]);
+            int idx = abs(nums[i]) - 1;
+            if(nums[idx] > 0) {
+                nums[idx] *= -1;
             }
         }
 
         for(int i = 0; i < n; i++) {
-            if(i != nums[i]-1) {
+            if(nums[i] > 0) {
                 ans.push_back(i+1);
             }
         }
+
         return ans;
     }
 };
