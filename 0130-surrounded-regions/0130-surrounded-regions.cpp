8758ccb7e2a289a80bad7289c3405322e1fcb36d
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-01 22:13:41 +0530

    Time: 3 ms (21.62%), Space: 13.8 MB (97.64%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -6,7 +6,7 @@ public:
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(board[i][j] == 'O' && (i == 0 || j == 0 || i == board.size()-1 || j == board[0].size()-1)) {
+                if(board[i][j] == 'O' && (i == 0 || j == 0 || i == m-1 || j == n-1)) {
                     DFS(board, i, j);
                 }
             }
@@ -14,23 +14,25 @@ public:
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(board[i][j] == 'O') board[i][j] = 'X';
+                if(board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
             }
         }
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(board[i][j] == 'T') board[i][j] = 'O';
+                if(board[i][j] == 'T') {
+                    board[i][j] = 'O';
+                }
             }
         }
     }
 
     void DFS(vector<vector<char>>& board, int i, int j) {
-        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size()) {
+        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] == 'X' || board[i][j] == 'T') {
             return;
         }
-
-        if(board[i][j] == 'X' || board[i][j] == 'T') return;
 
         board[i][j] = 'T';
         DFS(board, i+1, j);
