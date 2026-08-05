5553589f035debb000ad1d73bf4a1224c4fa170b
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-05 14:32:46 +0530

    Time: 0 ms (100%), Space: 13.9 MB (90.88%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -14,26 +14,24 @@ public:
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(board[i][j] == 'O') {
-                    board[i][j] = 'X';
-                }
+                if(board[i][j] == 'O') board[i][j] = 'X';
             }
         }
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(board[i][j] == 'T') {
-                    board[i][j] = 'O';
-                }
+                if(board[i][j] == 'T') board[i][j] = 'O';
             }
         }
     }
 
     void DFS(vector<vector<char>>& board, int i, int j) {
-        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] == 'X' || board[i][j] == 'T') {
+        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] == 'X') {
             return;
         }
 
+        if(board[i][j] == 'T') return;
+        
         board[i][j] = 'T';
         DFS(board, i+1, j);
         DFS(board, i-1, j);
