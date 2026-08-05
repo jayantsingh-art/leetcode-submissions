6755cacffe67c35dadbb353abddf4fefeb0b6582
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-05 22:31:52 +0530

    Time: 12 ms (82.73%), Space: 23.5 MB (92.72%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,31 +2,35 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        int distance = 1;
+        int ans = 1;
         queue<pair<int, int>> q;
 
-        vector<pair<int, int>> directions = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+
+        int directions[8][2] = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
         q.push({0, 0});
 
         while(!q.empty()) {
+
             int size = q.size();
 
-            for(int i = 0; i < size; i++) {
+            while(size--) {
                 auto [x, y] = q.front();
                 q.pop();
-                if(x == n-1 && y == n-1) return distance;
-                for(auto dir:directions) {
-                    int row = x + dir.first;
-                    int col = y + dir.second;
+                if(x == n-1 && y == n-1) return ans;
 
-                    if(row >= 0 && col >= 0 && row < n && col < n && grid[row][col] == 0) {
-                        grid[row][col] = 1;
-                        q.push({row, col});
+                for(auto dir : directions) {
+                    int newRow = x + dir[0];
+                    int newCol = y + dir[1];
+
+                    if(newRow >= 0 && newCol >= 0 && newRow < n && newCol < n && grid[newRow][newCol] == 0) {
+                        q.push({newRow, newCol});
+                        grid[newRow][newCol] = 1;
                     }
                 }
             }
-            distance++;
+            ans++;
+            // cout << ans << endl;
         }
         return -1;
     }
