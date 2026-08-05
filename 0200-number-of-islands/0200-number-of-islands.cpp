375bc63b7472645bad9ec3277f72e1c8fcf7309b
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-05 11:46:47 +0530

    Time: 22 ms (88.62%), Space: 16.2 MB (90.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -13,12 +13,11 @@ public:
                 }
             }
         }
-
         return ans;
     }
 
     void DFS(vector<vector<char>>& grid, int i, int j) {
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0') {
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0') {
             return;
         }
 
