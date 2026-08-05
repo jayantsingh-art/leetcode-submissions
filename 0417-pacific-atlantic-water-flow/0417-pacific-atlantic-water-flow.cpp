cde48b0ea2eea7d0fcf361fe93c53838cb98e864
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-06 00:33:10 +0530

    Time: 7 ms (57.43%), Space: 22.2 MB (89.2%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -4,25 +4,21 @@ public:
         int m = heights.size();
         int n = heights[0].size();
         vector<vector<int>> ans;
-        vector<vector<bool>> pacific(m, vector<bool>(n, 0));
-        vector<vector<bool>> atlantic(m, vector<bool>(n, 0));
+        vector<vector<bool>> pacific(m, vector<bool>(n, false));
+        vector<vector<bool>> atlantic(m, vector<bool>(n, false));
 
-        //first column
         for(int i = 0; i < m; i++) {
             DFS(heights, pacific, i, 0);
         }
 
-        //first row
         for(int j = 0; j < n; j++) {
             DFS(heights, pacific, 0, j);
         }
 
-        //last column
         for(int i = m-1; i >= 0; i--) {
             DFS(heights, atlantic, i, n-1);
         }
 
-        //last row
         for(int j = n-1; j >= 0; j--) {
             DFS(heights, atlantic, m-1, j);
         }
@@ -34,14 +30,16 @@ public:
                 }
             }
         }
+
         return ans;
     }
 
     void DFS(vector<vector<int>>& heights, vector<vector<bool>>& visited, int i, int j) {
         if(visited[i][j]) return;
+
         visited[i][j] = true;
 
-        vector<pair<int, int>> directions = {
+        int directions[4][2] = {
             {1, 0},
             {-1, 0},
             {0, 1},
@@ -49,11 +47,11 @@ public:
         };
 
         for(auto dir : directions) {
-            int newRow = i + dir.first;
-            int newCol = j + dir.second;
+            int row = i + dir[0];
+            int col = j + dir[1];
 
-            if(newRow >= 0 && newCol >= 0 && newRow < heights.size() && newCol < heights[0].size() && heights[newRow][newCol] >= heights[i][j]) {
-                DFS(heights, visited, newRow, newCol);
+            if(row >= 0 && col >= 0 && row < heights.size() && col < heights[0].size() && heights[row][col] >= heights[i][j]) {
+                DFS(heights, visited, row, col);
             }
         }
     }
