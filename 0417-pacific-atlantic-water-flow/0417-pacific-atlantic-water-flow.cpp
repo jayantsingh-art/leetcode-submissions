97db9aa531a70e6df5a683f496fee843b36aa675
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-07 15:14:25 +0530

    Time: 2 ms (89.73%), Space: 22 MB (98.87%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -3,10 +3,9 @@ public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int m = heights.size();
         int n = heights[0].size();
-        vector<vector<int>> ans; 
-
-        vector<vector<bool>> pacific(m, vector<bool>(n, 0));
-        vector<vector<bool>> atlantic(m, vector<bool>(n, 0));
+        vector<vector<int>> ans;
+        vector<vector<bool>> pacific(m, vector<bool>(n, false));
+        vector<vector<bool>> atlantic(m, vector<bool>(n, false));
 
         for(int i = 0; i < m; i++) {
             DFS(heights, pacific, i, 0);
@@ -16,11 +15,11 @@ public:
             DFS(heights, pacific, 0, j);
         }
 
-        for(int i = m-1; i >= 0; i--) {
+        for(int i = 0; i < m; i++) {
             DFS(heights, atlantic, i, n-1);
         }
 
-        for(int j = n-1; j >= 0; j--) {
+        for(int j = 0; j < n; j++) {
             DFS(heights, atlantic, m-1, j);
         }
 
@@ -40,18 +39,13 @@ public:
 
         visited[i][j] = true;
 
-        int directions[4][2] = {
-            {1, 0},
-            {-1, 0},
-            {0, 1},
-            {0, -1}
-        };
+        int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
         for(auto dir : directions) {
             int row = i + dir[0];
             int col = j + dir[1];
 
-            if(row >= 0 && col >= 0 && row < heights.size() && col < heights[0].size() && heights[row][col] >= heights[i][j]) {
+            if(row >= 0 && col >= 0 && row < heights.size() && col < heights[0].size() && heights[i][j] <= heights[row][col]) {
                 DFS(heights, visited, row, col);
             }
         }
