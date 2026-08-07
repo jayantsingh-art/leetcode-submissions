16ca43c998f7ae89ff531dac8074c173e72e2af3
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-07 16:34:10 +0530

    Time: 16 ms (58.77%), Space: 23.7 MB (82.75%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        int n = grid.size();
+        int m = grid.size();
+        int n = grid[0].size();
         int land = 0, distance = 0;
         queue<pair<int, int>> q;
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(grid[i][j] == 1) {
                     q.push({i, j});
@@ -14,13 +15,7 @@ public:
             }
         }
 
-        if(land == 0 || land == n*n) {
-            return -1;
-        }
-
-        int directions[4][2] = {
-            {1,0}, {-1,0}, {0,1}, {0,-1}
-        };
+        if(land == n*n || land == 0) return -1;
 
         while(!q.empty()) {
             int size = q.size();
@@ -28,12 +23,15 @@ public:
             while(size--) {
                 auto [x, y] = q.front();
                 q.pop();
+                if(land == n*n) return grid[x][y];
+
+                int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
                 for(auto dir : directions) {
                     int i = x + dir[0];
                     int j = y + dir[1];
 
-                    if(i >= 0 && j >= 0 && i < n && j < n && grid[i][j] == 0) {
+                    if(i >= 0 && j >= 0 && i < grid.size() && j < grid[0].size() && grid[i][j] == 0) {
                         grid[i][j] = 1;
                         q.push({i, j});
                     }
@@ -41,7 +39,6 @@ public:
             }
             distance++;
         }
-
         return distance-1;
     }
 };
