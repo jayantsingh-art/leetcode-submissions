114b466f649e6b38beb3646e92ff50464a31918f
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-10 16:13:54 +0530

    Time: 1 ms (36.37%), Space: 16.6 MB (97.87%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,8 +3,9 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        queue<pair<int, int>> q;
         int freshOranges = 0, minutes = 0;
+
+        queue<pair<int, int>> q;
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
@@ -17,31 +18,30 @@ public:
         }
 
         if(freshOranges == 0) return 0;
-        if(q.empty()) return -1;
 
         int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
         while(!q.empty()) {
             int size = q.size();
-            // cout << "Queue size = " << size << endl;
-            while(size--) {
-                auto [row, col] = q.front();
-                q.pop();
-                // cout << "Processing (" << row << "," << col << ")" << endl;
-                for(auto dir : directions) {
-                    int newRow = row + dir[0];
-                    int newCol = col + dir[1];
 
-                    if(newRow >= 0 && newCol >= 0 && newRow < m && newCol < n && grid[newRow][newCol] == 1) {
-                        grid[newRow][newCol] = 2;
-                        q.push({newRow, newCol});
+            while(size--) {
+                auto [x, y] = q.front();
+                q.pop();
+
+                for(auto dir : directions) {
+                    int i = x + dir[0];
+                    int j = y + dir[1];
+
+                    if(i >= 0 && j >= 0 && i < m && j < n && grid[i][j] == 1) {
+                        grid[i][j] = 2;
+                        q.push({i, j});
                         freshOranges--;
-                        // cout << "Fresh: " << freshOranges << endl;
                     }
                 }
             }
             minutes++;
         }
+
         return freshOranges == 0 ? minutes-1 : -1;
     }
 };
