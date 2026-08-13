7f4037335a84a56bbdef09461be6a9b5dde7d731
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-13 23:42:07 +0530

    Time: 12 ms (12.22%), Space: 28.6 MB (40.18%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -5,10 +5,10 @@ public:
         stack<int> st;
         vector<int> ans(n, -1);
 
-        for(int i = 2*n-1; i >= 0; i--) {
+        for(int i = 2*n - 1; i >= 0; i--) {
             int idx = i % n;
 
-            while(!st.empty() && nums[idx] >= nums[st.top()]) {
+            while(!st.empty() && nums[st.top()] <= nums[idx]) {
                 st.pop();
             }
 
