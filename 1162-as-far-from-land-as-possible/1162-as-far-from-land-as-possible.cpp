8b7493a5b06d65c55c1605afec19bf75c312410c
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-13 16:12:32 +0530

    Time: 8 ms (93.57%), Space: 23.7 MB (82.07%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
         int n = grid.size();
-        int land = 0, distance = 0;
         queue<pair<int, int>> q;
+        int land = 0, level = 0;
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
@@ -18,12 +18,14 @@ public:
             return -1;
         }
 
-        int directions[4][2] = {{1, 0}, {-1, 0}, {0,1}, {0, -1}};
+        int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
         while(!q.empty()) {
             int size = q.size();
-
-            if(land == n*n) return distance;
+            
+            if(land == n*n) {
+                return level;
+            }
 
             while(size--) {
                 auto [x, y] = q.front();
@@ -34,15 +36,14 @@ public:
                     int j = y + dir[1];
 
                     if(i >= 0 && j >= 0 && i < n && j < n && grid[i][j] == 0) {
-                        q.push({i, j});
                         grid[i][j] = 1;
+                        q.push({i, j});
                         land++;
                     }
                 }
             }
-            distance++;
+            level++;
         }
-
         return -1;
     }
 };
