460df4109ddb843df3e6e01884f5c62f10553143
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-15 01:22:09 +0530

    Time: 3 ms (82.53%), Space: 46.3 MB (63.55%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -20,17 +20,19 @@ public:
             curr = curr->next;
         }
 
-        vector<int> ans(nums.size(), 0);
-
         for(int i = 0; i < nums.size(); i++) {
-            while(!st.empty() && nums[st.top()] < nums[i]) {
-                ans[st.top()] = nums[i];
+            while(!st.empty() && nums[i] > nums[st.top()]) {
+                nums[st.top()] = nums[i];
                 st.pop();
             }
-
             st.push(i);
         }
 
-        return ans;
+        while(!st.empty()) {
+            nums[st.top()] = 0;
+            st.pop();
+        }
+
+        return nums;
     }
 };
