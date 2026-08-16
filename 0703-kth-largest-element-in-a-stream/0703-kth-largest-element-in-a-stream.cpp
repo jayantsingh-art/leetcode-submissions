f296c0bb0b8a4bbe3a66673bef9277ffc772efb0
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-16 13:51:52 +0530

    Time: 3 ms (90.86%), Space: 33.1 MB (26.34%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -4,18 +4,16 @@ class KthLargest {
 public:
     KthLargest(int k, vector<int>& nums) {
         K = k;
-
         for(int num : nums) {
             pq.push(num);
             if(pq.size() > K) pq.pop();
         }
+        
     }
     
     int add(int val) {
         pq.push(val);
-        if(pq.size() > K) {
-            pq.pop();
-        }
+        if(pq.size() > K) pq.pop();
 
         return pq.top();
     }
