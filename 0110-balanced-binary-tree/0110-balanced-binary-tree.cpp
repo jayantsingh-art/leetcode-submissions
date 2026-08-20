57fbecaf411a291b6cb16fcab4befe8c1c3cd95a
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-20 07:44:27 +0530

    Time: 0 ms (100%), Space: 23.1 MB (55.77%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,21 +12,22 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        if(!root) return true;
-
         return height(root) != -1;
     }
 
     int height(TreeNode* root) {
+        
         if(!root) return 0;
 
-        if(!root->left && !root->right) return 1;
-
         int left = height(root->left);
-        if(left == -1) return -1;
+        if(left == -1) {
+            return -1;
+        }
 
         int right = height(root->right);
-        if(right == -1) return -1;
+        if(right == -1) {
+            return -1;
+        }
 
         if(abs(left - right) > 1) return -1;
 
