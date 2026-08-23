ba077031ab7154363ae77b90e325b75a8ab04ce3
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-23 15:11:27 +0530

    Time: 0 ms (100%), Space: 10.7 MB (24.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n, 0);
+        vector<int> ans(n);
 
         if(n == 1) return nums[0];
         if(n == 2) return max(nums[0], nums[1]);
@@ -11,7 +11,7 @@ public:
         ans[1] = max(nums[0], nums[1]);
 
         for(int i = 2; i < n; i++) {
-            ans[i] = max(ans[i-1], nums[i] + ans[i-2]);
+            ans[i] = max(nums[i] + ans[i-2], ans[i-1]);
         }
 
         return ans[n-1];
