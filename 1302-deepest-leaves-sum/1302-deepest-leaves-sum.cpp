9a1edc76d62de058e2aa94310ebdc8ed45733e8a
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-08-23 09:09:58 +0530

    Time: 0 ms (100%), Space: 62.6 MB (31.32%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -14,16 +14,19 @@ public:
     int deepestLeavesSum(TreeNode* root) {
         int sum = 0;
         queue<TreeNode*> q;
+
+        if(!root) return 0;
         q.push(root);
-        
+
         while(!q.empty()) {
             int size = q.size();
             sum = 0;
+
             while(size--) {
                 TreeNode* node = q.front();
                 q.pop();
-
                 sum += node->val;
+
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
             }
