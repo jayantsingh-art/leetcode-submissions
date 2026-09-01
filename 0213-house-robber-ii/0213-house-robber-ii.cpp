921d55ae12b25726f081cfaf97ad8dc0bff6991e
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-09-02 00:34:17 +0530

    Time: 0 ms (100%), Space: 10.7 MB (61.28%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -6,19 +6,18 @@ public:
 
         if(n == 1) return nums[0];
         if(n == 2) return max(nums[0], nums[1]);
-        
+
         ans1[0] = nums[0];
         ans1[1] = max(nums[0], nums[1]);
 
         for(int i = 2; i < n-1; i++) {
-            ans1[i] = max(nums[i] + ans1[i-2], ans1[i-1]);
+            ans1[i] = max(ans1[i-1], ans1[i-2] + nums[i]);
         }
 
-        ans2[0] = 0;
         ans2[1] = nums[1];
 
         for(int i = 2; i < n; i++) {
-            ans2[i] = max(nums[i] + ans2[i-2], ans2[i-1]);
+            ans2[i] = max(ans2[i-1], ans2[i-2] + nums[i]);
         }
 
         return max(ans1[n-2], ans2[n-1]);
