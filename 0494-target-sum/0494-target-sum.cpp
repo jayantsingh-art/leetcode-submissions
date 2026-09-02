3ecec2cb5297e6aed88e734cb256b423857eecb6
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-09-02 13:11:03 +0530

    Time: 7 ms (67.06%), Space: 15.7 MB (39.1%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -9,20 +9,19 @@ public:
         }
 
         if(abs(target) > sum) return 0;
-        if((sum + target) % 2 != 0) return 0;
+        if((target + sum) % 2 != 0) return 0;
 
-        target = (sum+target) / 2;
+        target = (sum + target) / 2;
+        vector<vector<int>> dp(n+1, vector<int>(target+1));
 
-        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
-
-        for(int i = 0; i <= n; i++) {
+        for(int i = 0; i < n+1; i++) {
             dp[i][0] = 1;
         }
 
         for(int i = 1; i <= n; i++) {
             for(int j = 0; j <= target; j++) {
                 if(nums[i-1] <= j) {
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j - nums[i-1]];
                 } else {
                     dp[i][j] = dp[i-1][j];
                 }
