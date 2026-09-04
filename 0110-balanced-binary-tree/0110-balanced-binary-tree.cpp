26666a6c76dc2f0bd66eeed4020131b440f9edad
jayantsingh-art <jayantsingh97.in@gmail.com>
2026-09-04 16:44:17 +0530

    Time: 0 ms (100%), Space: 23 MB (83.72%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        return height(root) != -1;
+        return height(root) == -1 ? false : true;
     }
 
     int height(TreeNode* root) {
